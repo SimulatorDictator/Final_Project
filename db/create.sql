fa91dd8b8ab5b019cbd73de0239d93e53feff579
Ubuntu <azureuser@FinalProject.j10jjmnoi0ne1ojpcm0ochpyje.dx.internal.cloudapp.net>
2022-10-24 13:55:52 +0000

    edited create.sql

--- db/create.sql
+++ db/create.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Games (
         gid INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR() 
+        name VARCHAR(30) 
 );
 CREATE TABLE IF NOT EXISTS Customers (
         cid INT PRIMARY KEY AUTO_INCREMENT, 
-        name VARCHAR(),
+        name VARCHAR(30),
         table INT,
         fk_gid INT, 
         FOREIGN KEY (fk_gid) REFERENCES Games(id)
