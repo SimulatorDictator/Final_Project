9899b07dcf94b811716303f4fc7f3d7e8a9d8139
Ubuntu <azureuser@FinalProject.j10jjmnoi0ne1ojpcm0ochpyje.dx.internal.cloudapp.net>
2022-10-25 14:29:32 +0000

    Working dockercompose with working app

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -5,9 +5,9 @@ from os import getenv
 app = Flask(__name__, template_folder="templates")
 
 # Sets up your database. We are using SQLAlchemy's sqlite. 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:password@mysql:3306/flask-db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql:3306/flask-db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+#app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
